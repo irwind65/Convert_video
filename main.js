d917ad8a990d47b989785338632f11c559151809
irwind65 <irwind65@gmail.com>
2020-03-22 17:26:01 -0400

    Video converter code
    Irwin's code for Stephen Grider's Electron for Desktop Apps: convert video (section 5).

---
+++ main.js
@@ -0,0 +1,50 @@
+const electron = require('electron');
+const { app, BrowserWindow, ipcMain } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+const _ = require('lodash');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    title: 'Video Converter',
+    webPreferences: {
+      backgroundThrottling: false,
+      nodeIntegration: true
+    }
+  });
+  mainWindow.loadURL(`file://${__dirname}/src/index.html`);
+});
+
+ipcMain.on('videos:added', (event, videos) => {
+  const promises = _.map(videos, video => {
+    return new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(video.path, (err, metadata) => {
+        video.duration = metadata.format.duration;
+        video.format = 'avi';
+        resolve(video);
+      });
+    });
+  });
+
+  Promise.all(promises).then(results => {
+    mainWindow.webContents.send('metadata:complete', results);
+  });
+});
+
+ipcMain.on('conversion:start', (event, videos) => {
+  _.each(videos, video => {
+    const outputDirectory = video.path.split(video.name[0]);
+    const outputName = video.name.split('.')[0];
+    const outputPath = `${outputDirectory}${outputName}.${video.format}`;
+
+    ffmpeg(video.path)
+      .output(outputPath)
+      .on('end', () =>
+        mainWindow.webContents.send('conversion:end', { video, outputPath })
+      )
+      .run();
+  });
+});
